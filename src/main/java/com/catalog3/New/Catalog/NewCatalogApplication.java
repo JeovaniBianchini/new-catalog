375f24da5e961e281355c94050633b9ce2487aa7
Jeovani Bianchini <bianchini.jeovani@gmail.com>
2025-02-10 20:09:45 -0400

    First commit

---
+++ src/main/java/com/catalog3/New/Catalog/NewCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.catalog3.New.Catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/catalog3/New/Catalog/NewCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catalog3.New.Catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
