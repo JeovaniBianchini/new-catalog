1f014bb21128f7a7d312cb5d1bf40434b02a602f
Jeovani Bianchini <bianchini.jeovani@gmail.com>
2025-02-11 20:42:47 -0400

    Category FindById, Exception

--- src/main/java/com/catalog3/New/Catalog/services/CategoryService.java
+++ src/main/java/com/catalog3/New/Catalog/services/CategoryService.java
@@ -3,11 +3,13 @@ package com.catalog3.New.Catalog.services;
 import com.catalog3.New.Catalog.dtos.CategoryDto;
 import com.catalog3.New.Catalog.entities.Category;
 import com.catalog3.New.Catalog.repositories.CategoryRepository;
+import com.catalog3.New.Catalog.services.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -20,5 +22,12 @@ public class CategoryService {
     public List<CategoryDto> findAll(){
         List<Category> list = categoryRepository.findAll();
         return list.stream().map(x -> new CategoryDto(x)).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDto findById(Long id) {
+        Optional<Category> obj = categoryRepository.findById(id);
+        Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+        return new CategoryDto(entity);
     }
 }
